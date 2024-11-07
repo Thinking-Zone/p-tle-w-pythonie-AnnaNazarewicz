3bc5976e34672682fff3777c5e8523a1ad395096
AnnaNazarewicz <anna.nazarewicz@thinkingzone.pl>
2024-11-07 10:03:30 +0100

    Update zadanie6.py

---
+++ zadanie6.py
@@ -0,0 +1,3 @@
+# Pętla 'for' jest zwykle używana, gdy z góry zaczynamy liczbe iteracji,
+# np. iteracja po elementach w kolekcji (liście, zakresie it.) lub określonym zakresie liczb.
+# Pętla 'for' jest bardziej delektywna i czytelna gdy iternujemy po znanym zbiorze lub przedziale 
